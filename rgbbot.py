06f77ba534934a7ac7c9294a2f5d26cbb277bcda
IceJonas <32646566+IceJonas@users.noreply.github.com>
2020-11-15 11:02:23 +0000

    Add files via upload

---
+++ rgbbot.py
@@ -0,0 +1,29 @@
+from pyautogui import *
+import pyautogui
+import time
+import keyboard
+import random
+import win32api, win32con
+
+# Cords
+y1 = 1170
+x1 = 775
+x2 = 925
+x3 = 1080
+x4 = 1240
+
+def click(x,y):
+    win32api.SetCursorPos((x,y))
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,0,0)
+    time.sleep(0.01)
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0)
+
+while keyboard.is_pressed('q') == False:
+    if pyautogui.pixel(775,1170)[0] == 0:
+        click(775,1170)
+    if pyautogui.pixel(925,1170)[0] == 0:
+        click(925,1170)
+    if pyautogui.pixel(1080,1170)[0] == 0:
+        click(1080,1170)
+    if pyautogui.pixel(1240,1170)[0] == 0:
+        click(1240,1170)
